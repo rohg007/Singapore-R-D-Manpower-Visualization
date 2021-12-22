9acbfc6a1f64f999d99f8f81c7cf3795bf95fb8c
Rohan Gupta <gupta.rohan@live.com>
2021-12-23 02:50:12 +0530

    added line chart

--- visualization.js
+++ visualization.js
@@ -10,13 +10,25 @@ const width = 700;
 
 function showData(data) {
   this.data = data;
-  loadYears(data);
+  loadYears();
+  loadSectors();
   change();
 }
 
 function change() {
+  let chartType = document.getElementById("chart_type").value;
   let selectedYear = document.getElementById("year").value;
-  drawBarChart(getDataByYear(selectedYear), selectedYear);
+  let selectedSector = document.getElementById("sector").value;
+
+  if (chartType === "vis1") {
+    hideElement("box_sector");
+    showElement("box_year");
+    drawBarChart(getDataByYear(selectedYear), selectedYear);
+  } else if (chartType === "vis2") {
+    hideElement("box_year");
+    showElement("box_sector");
+    drawLineChart(getDataBySector(selectedSector), selectedSector);
+  }
 }
 
 function drawBarChart(appData, year) {
@@ -101,15 +113,111 @@ function drawBarChart(appData, year) {
     .on("mouseout", mouseleave);
 }
 
-function loadYears(data) {
+function drawLineChart(appData, sector) {
+  d3.selectAll("svg > *").remove();
+  console.log(appData);
+  drawspace
+    .append("text")
+    .attr("transform", "translate(0,0)")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("font-size", "24px")
+    .text(sector + " FTE trend from 2010 to 2018");
+
+  let parseDate = d3.timeParse("%Y");
+
+  var xScale = d3
+    .scaleTime()
+    .range([0, width])
+    .domain(d3.extent(appData, (d) => parseDate(d.year)));
+
+  var yScale = d3
+    .scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(appData, (d) => +d.fte)])
+    .nice();
+
+  var g = drawspace
+    .append("g")
+    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+  g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale))
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-45)");
+
+  g.append("g")
+    .call(d3.axisLeft(yScale))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0)
+    .attr("x", -150)
+    .attr("dy", "-5.1em")
+    .attr("text-anchor", "end")
+    .attr("stroke", "black")
+    .text("Full-Time Equivalence");
+
+  g
+    .append("path")
+    .datum(appData)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x((d) => xScale(parseDate(d.year)))
+        .y((d) => yScale(d.fte))
+    );
+}
+
+function loadYears() {
   const years = [...new Set(data.map((d) => d.year))].sort();
   let sel = "";
   years.forEach((y) => (sel += `<option value="${y}">${y}</option>`));
   document.getElementById("year").innerHTML = sel;
 }
 
+function loadSectors() {
+  const sectors = [...new Set(data.map((d) => d.sector))];
+  let sel = "";
+  sectors.forEach((y) => (sel += `<option value="${y}">${y}</option>`));
+  document.getElementById("sector").innerHTML = sel;
+}
+
 function getDataByYear(year) {
   return data.filter((d) => {
     return d.year === year;
   });
+}
+
+function getDataBySector(sector) {
+  let sectorData = {};
+  for (let d of data) {
+    let year = d.year;
+    if (d.sector === sector) {
+      if (year in sectorData) sectorData[year] += parseFloat(d.fte);
+      else sectorData[year] = parseFloat(d.fte);
+    }
+  }
+  console.log(sectorData);
+  const arr = Object.keys(sectorData).map((key) => ({
+    year: key,
+    fte: sectorData[key],
+  }));
+  console.log(arr);
+  return arr;
+}
+
+function hideElement(element) {
+  document.getElementById(element).style.display = "none";
+}
+
+function showElement(element) {
+  document.getElementById(element).style.display = "block";
 }
